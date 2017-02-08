3006e6d119bcdd5f0a69edf443023b4983ec410f
aberry5621 <aberry5621@gmail.com>
2017-02-07 21:28:44 -0800

    Initial Commit

---
+++ COMP 235 Homework 02 Problem 02/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  COMP 235 Homework 02 Problem 02
+//
+//  Created by ax on 2/7/17.
+//  Copyright © 2017 COMP235. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
